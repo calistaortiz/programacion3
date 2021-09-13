f9a75d739ae936ff2b9f79bcec07e2a2e709e059
valudrechsler <valudrech@gmail.com>
2021-09-12 22:25:00 -0300

    ver m

--- src/components/tarjetas/Tarjetas.js
+++ src/components/tarjetas/Tarjetas.js
@@ -7,8 +7,22 @@ class Tarjetas extends Component {
     constructor(props){
         super(props)
         this.state = {
+            text: "Ver más",
+            viewMore: false
+        }
+    }
 
-            
+    viewMore(){
+        if(this.state.viewMore){
+            this.setState({
+                text:'Ver más',
+                viewMore: false
+            })
+        }else{
+            this.setState({
+                text: 'Ver menos',
+                viewMore: true,
+            })
         }
     }
 
@@ -18,14 +32,14 @@ render(){
                             
                             <img src={`https://image.tmdb.org/t/p/w500${this.props.info.poster_path}`} alt="" />
                             <h3>{this.props.info.title}</h3>
-                            <p className="description">{this.props.info.overview}</p>
+                            <p className="description">DESCRIPCIÓN: {this.props.info.overview}</p>
                             <section className="aditional-info">
-                                <p>Original Language: {this.props.info.original_language}</p>
-                                <p>Rating: {this.props.info.vote_average}</p>
-                                <p>Premiere: {this.props.info.release_date}</p>
-                                <button className='more' onClick={()=> this.props.remove(this.props.info.id)}> borrar</button>
+                                <p >ORIGINAL LANGUAGE: {this.props.info.original_language}</p>
+                                <p className={` ${this.state.viewMore ? 'show' : 'hide'}`}>RATING: {this.props.info.vote_average}</p>
+                                <p className={ this.state.viewMore ? '' : 'hide' }>PREMIERE: {this.props.info.release_date}</p>
+                                <button className='remove' onClick={()=> this.props.remove(this.props.info.id)}> borrar</button>
                             </section>
-                            <a href="">Ver más</a>
+                            <button className='more' onClick={ () => this.viewMore()} >{this.state.text}</button>
                         </div>
                     
     )
