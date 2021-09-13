35e0e787656e8158f22780e7440d7d7202263889
valudrechsler <valudrech@gmail.com>
2021-09-13 10:26:06 -0300

    no anda

--- src/components/formulario/Formulario.js
+++ src/components/formulario/Formulario.js
@@ -23,8 +23,8 @@ class Formulario extends Component{
     render(){
         return(
          <div className='buscar'>  
-            <form onSubmit={(event)=>this.evitarSubmit(event)}>
-               <input type="text" onChange={(event)=>this.controlarCambios(event)} value={this.state.valor} placehoder='buscar'/>
+            <form name='search' placeholder='buscar' onSubmit={(event)=>this.evitarSubmit(event)}>
+               <input type="text" name="search" onChange={(event)=>this.controlarCambios(event)} value={this.state.valor} placehoder='search'/>
             </form>
         </div>
         )
